c8711e9a9f7887a5dd4ad1a83bdc9eb525c414a0
Alex Nordstrom <a.l.nordstrom@gmail.com>
2021-05-10 23:55:10 -0400

    ugly but working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const util = require('./util')
 const mapParser = require('./mapParser')
 const renderer = require('./renderer')
 
@@ -20,7 +21,7 @@ client.on('message', async msg => {
     !msg.author.bot,
     msg.content.startsWith(prefix)
   ]
-  for(let condition in conditions)
+  for(let condition of conditions)
     if(!condition)
       return
   
@@ -37,19 +38,47 @@ client.on('message', async msg => {
       game = {
         history: [map],
         players: [],
-        turn: null
+        turn: 0
       }
-      msg.channel.send(renderer.render(history.slice(-1)))
+      msg.channel.send(util.turnMsg(game) + renderer.render(game.history[game.history.length-1]))
     } catch(err) {
-      if(err === 'mapNotFound')
-        msg.reply('Map not found. Valid maps are: ', mapParser.list.join(', '))
-      else
+      if(err === 'mapNotFound') {
+        let list = await mapParser.list()
+        msg.channel.send(`Map not found. Valid maps are: ${list.join(', ')}`)
+      } else
         throw err
     }
   }
 
   if(command === 'move') {
-    
+    if(game.turn === null)
+      msg.channel.send('Start a game first.')
+    else {
+      let state = JSON.parse(JSON.stringify(game.history[game.history.length-1]))
+      let headings = args[0].split(':')
+      let [from, to] = headings.map(heading => util.getFromHeading(state, heading))
+      state[to.row][to.file] = {
+        type: state[from.row][from.file].type,
+        team: state[from.row][from.file].team
+      }
+      state[from.row][from.file] = null
+      game.history.push(state)
+      game.turn = game.turn==0 ? 1 : 0
+      msg.channel.send(util.turnMsg(game) + renderer.render(game.history[game.history.length-1]))
+    }
+  }
+
+  if(command === 'undo') {
+    game.history.pop()
+    game.turn = game.turn==0 ? 1 : 0
+    msg.channel.send(util.turnMsg(game) + renderer.render(game.history[game.history.length-1]))
+  }
+
+  if(command === 'test') {
+    //let map = await mapParser.parse(args[0])
+    let list = await mapParser.list()
+    //msg.channel.send(`\`\`\`\n${map.join('\n')}\`\`\``)
+    msg.channel.send(list.join(', '))
   }
 })
 client.login(process.env.TOKEN)
--- src/mapParser.js
+++ src/mapParser.js
@@ -1,8 +1,45 @@
+const fs = require('fs').promises
+const path = require('path')
+
+let key = {
+  a: { type: 'artillery', team: 1 },
+  c: { type: 'command', team: 1 },
+  i: { type: 'infantry', team: 1 },
+  s: { type: 'sniper', team: 1 },
+  t: { type: 'tank', team: 1 },
+  A: { type: 'artillery', team: 0 },
+  C: { type: 'command', team: 0 },
+  I: { type: 'infantry', team: 0 },
+  S: { type: 'sniper', team: 0 },
+  T: { type: 'tank', team: 0 },
+}
+
 module.exports = {
   parse: (mapName) => {
-
+    return fs.readFile(
+      path.resolve(__dirname, `./maps/${mapName}.json`),
+      'utf8'
+    ).then(file => {
+      let json = JSON.parse(file)
+      let map = json.map(line => line.split('').map(item => {
+        if(item in key)
+          return {
+            type: key[item].type,
+            team: key[item].team
+          }
+        else
+          return null
+      }))
+      return map
+    }).catch(err => {
+      console.log(err)
+      throw('mapNotFound')
+    })
   },
+  // return array of available maps
   list: () => {
-    
+    return fs.readdir(
+      path.resolve(__dirname, `./maps`)
+    ).then(files => files.map(file => file.split('.')[0]))
   }
 }
