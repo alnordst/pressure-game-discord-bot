bfc3db87214bfc3b009509e2d392322931663926
Alex Nordstrom <a.l.nordstrom@gmail.com>
2021-06-25 14:30:06 -0400

    more logging, fix webhook?

--- src/util/handleWebhook.js
+++ src/util/handleWebhook.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const api = require('./api')
-const { matchReport } = require('./render')
+const { matchReport } = require('./reports')
+const render = require('./render')
 
 let handleWebhook = client => {
   const app = express()
@@ -12,23 +13,28 @@ let handleWebhook = client => {
   })
 
   app.post('/', async (req, res) => {
-    console.log(`Handling webhook; reason: ${req.body.reason}; match_id: ${req.body.match_id}; player_id: ${req.body.player_id}`)
-    let player = await api.get(`/player/${req.body.player_id}`)
-    if(req.body.reason == 'draw-offer') {
-      client.users.cache.get(player.discord_id).send(`A draw was offered in match ${req.body.match_id}.`)
-    } else {
-      let match = await api.get(`/match/${req.body.match_id}`)
-      let head = ''
-      if(req.body.reason == 'match over')
-        head = 'Match is over!\n'
-      else if(req.body.reason == 'next turn')
-        head = 'Next turn!\n'
-      await render(client.users.cache.get(player.discord_id), {
-        text: head + matchReport(match),
-        board: JSON.parse(match.last_state.data)
-      })
+    try {
+      console.log(`Handling webhook; reason: ${req.body.reason}; match_id: ${req.body.match_id}; player_id: ${req.body.player_id}`)
+      let player = await api.get(`/player/${req.body.player_id}`)
+      if(req.body.reason == 'draw-offer') {
+        client.users.cache.get(player.discord_id).send(`A draw was offered in match ${req.body.match_id}.`)
+      } else {
+        let match = await api.get(`/match/${req.body.match_id}`)
+        let head = ''
+        if(req.body.reason == 'match over')
+          head = 'Match is over!\n'
+        else if(req.body.reason == 'next turn')
+          head = 'Next turn!\n'
+        await render(client.users.cache.get(player.discord_id), {
+          text: head + matchReport(match),
+          board: JSON.parse(match.last_state.data)
+        })
+      }
+      res.sendStatus(200)
+    } catch(err) {
+      console.log(err)
+      res.sendStatus(500)
     }
-    res.sendStatus(200)
   })
 
   app.listen(3001)
